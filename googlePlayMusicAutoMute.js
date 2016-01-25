33230521a9e77bf25ef93ace1f3539b000e01874
a-bort <bortron5000@gmail.com>
2016-01-25 12:50:03 -0500

    Return to original volume on unmute

--- googlePlayMusicAutoMute.js
+++ googlePlayMusicAutoMute.js
@@ -1,14 +1,17 @@
 console.log("Automute running");
 
-prevVolume = 0;
+window.prevVolume = 0;
 
 setInterval(function(){
 	var volumeSlider = document.getElementById("material-vslider");
 	var currentTitle = document.getElementById("currently-playing-title");
 	if(currentTitle.textContent == "We'll be right back"){
-		prevVolume = volumeSlider.getAttribute("value");
-		volumeSlider.setAttribute("value", 0);
+		var volume = volumeSlider.getAttribute("value");
+		if(volume != "0"){
+			window.prevVolume = volume;
+			volumeSlider.setAttribute("value", 0);
+		}
 	} else if(volumeSlider.getAttribute("value") == "0"){
-		volumeSlider.setAttribute("value", prevVolume);
+		volumeSlider.setAttribute("value", window.prevVolume);
 	}
 }, 200);
