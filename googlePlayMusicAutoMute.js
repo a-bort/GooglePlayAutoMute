64bdc542cb37cc036a934c18a7ae916049cbc57d
a-bort <bortron5000@gmail.com>
2016-01-25 09:44:40 -0500

    Capture the current user volume when muting
    Store the current volume when going to mute, and restore that volume level when coming out of mute.

--- googlePlayMusicAutoMute.js
+++ googlePlayMusicAutoMute.js
@@ -1,9 +1,14 @@
+console.log("Automute running");
+
+prevVolume = 0;
+
 setInterval(function(){
 	var volumeSlider = document.getElementById("material-vslider");
 	var currentTitle = document.getElementById("currently-playing-title");
 	if(currentTitle.textContent == "We'll be right back"){
-		volumeSlider.setAttribute("value", 0)
+		prevVolume = volumeSlider.getAttribute("value");
+		volumeSlider.setAttribute("value", 0);
 	} else if(volumeSlider.getAttribute("value") == "0"){
-		volumeSlider.setAttribute("value", 60)
+		volumeSlider.setAttribute("value", prevVolume);
 	}
-}, 200)
+}, 200);
